d322da8c2e6531a5b4dc8b53c7816af5bedd2138
mdmamun000 <mamun.adp@gmail.com>
2024-06-06 19:11:15 -0400

    TC

--- TestJavaExample/src/TestProject/QAJava.java
+++ TestJavaExample/src/TestProject/QAJava.java
@@ -1,9 +1,29 @@
 package TestProject;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QAJava {
 
 	public static void main(String[] args) {
-		System.out.println("TC added");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mdmam\\Downloads\\chromedriver-124\\chromedriver-win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://ebay.com/");
+		
+		String title = driver.getTitle();
+		System.out.println("the title is: "+title);
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		for (WebElement link : links) {
+			System.out.println(link.getText());
+			
+		}
+		
+		driver.close();
 
 	}
 
